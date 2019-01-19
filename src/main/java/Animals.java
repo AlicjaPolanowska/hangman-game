b0c143db8db6983a87df8614742c00665dd85377
= <=>
2019-01-19 21:19:20 +0100

    update Animals class

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class Animals {
+public class Animals implements Category {
+    public ArrayList<String> animals;
+    public Random rand;
+
+    public Animals (){
+        animals = new ArrayList<String>();
+        animals.add("sheep");
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("camel");
+        animals.add("crocodile");
+        animals.add("bear");
+        animals.add("cow");
+        animals.add("horse");
+        animals.add("pig");
+        animals.add("elephant");
+        animals.add("giraffe");
+        animals.add("goose");
+        animals.add("cock");
+        animals.add("hen");
+        animals.add("fox");
+        animals.add("wolf");
+        animals.add("elk");
+        animals.add("mouse");
+        animals.add("frog");
+        animals.add("kangaroo");
+        animals.add("ant");
+        animals.add("owl");
+        animals.add("ladybird");
+        animals.add("grasshopper");
+        animals.add("turtle");
+        animals.add("deer");
+        animals.add("monkey");
+        animals.add("rabbit");
+        animals.add("hippo");
+        animals.add("butterfly");
+        animals.add("bee");
+        rand = new Random();
+    }
+
+    public String randObject() {
+        int index = rand.nextInt(30);
+        return animals.get(index);
+    }
+
+    public String getCategory() {
+        return "Animals";
+    }
+
+
 }
